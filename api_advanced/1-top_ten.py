1988395c752e1ea36df3e8428007c238d4fa82fb
root <l.isheja@alustudent.com>
2025-10-20 03:19:48 -0700

    Fix all validation issues in top_ten function

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -10,18 +10,17 @@ def top_ten(subreddit):
     """
     Queries Reddit API and prints titles of first 10 hot posts
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'python:script:v1.0 (by /u/your_username)'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'alu-scripting-api-advanced'}
+    params = {'limit': 10}
+
+    response = requests.get(url, headers=headers, params=params, 
+                          allow_redirects=False)
     
-    try:
-        response = requests.get(url, headers=headers, params={'limit': 10}, allow_redirects=False)
-        
-        if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
-            for post in posts[:10]:
-                print(post['data']['title'])
-        else:
-            print("None")
-    except Exception:
-        print("None")
+    if response.status_code == 200:
+        data = response.json()
+        posts = data.get('data', {}).get('children', [])
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        print(None)

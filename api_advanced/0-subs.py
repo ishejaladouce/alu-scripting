08aafd107bcbcbef3f9abc4aef007002614e736b
root <l.isheja@alustudent.com>
2025-10-20 03:05:03 -0700

    Clean PEP8 whitespace issues in 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -9,16 +9,16 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Queries Reddit API and returns number of subscribers for a given subreddit
-    
+
     Args:
         subreddit (str): The subreddit to query
-        
+
     Returns:
         int: Number of subscribers, 0 if invalid subreddit
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'alu-scripting-api-advanced'}
-    
+
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
